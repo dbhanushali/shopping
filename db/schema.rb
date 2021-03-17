0e096643d6d9c37892adb6f89e76143ba1f578f7
Dipen Bhanushali <dipen.bhanushali@gmail.com>
2021-03-17 18:01:52 +0530

    menu_items added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_120158) do
+ActiveRecord::Schema.define(version: 2021_03_17_122917) do
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.float "price"
+    t.integer "menu_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
+  end
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
